366c3cf7c08f5f4937a8ceb7cb6a223d92d3e5c9
Waldemar <butriman@mail.ru>
2024-03-04 14:50:42 +0300

    Update convert-to-pdf.js

--- convert-to-pdf.js
+++ convert-to-pdf.js
@@ -3,7 +3,7 @@ const puppeteer = require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('YOUR_HTML_PAGE_URL', {waitUntil: 'networkidle2'}); // Replace YOUR_HTML_PAGE_URL with your page URL
+  await page.goto('https://butriman.github.io/', {waitUntil: 'networkidle2'}); // Replace YOUR_HTML_PAGE_URL with your page URL
   await page.pdf({path: 'output.pdf', format: 'A4'});
   await browser.close();
 })();
