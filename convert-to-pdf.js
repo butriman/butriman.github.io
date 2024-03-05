ec7335ac34e41182eec26f1aaa73b344eedd8de1
Waldemar <butriman@mail.ru>
2024-03-05 12:58:44 +0300

    Delete convert-to-pdf.js

--- convert-to-pdf.js
+++
@@ -1,9 +0,0 @@
-const puppeteer = require('puppeteer');
-
-(async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto('https://butriman.github.io/', {waitUntil: 'networkidle2'}); // Replace YOUR_HTML_PAGE_URL with your page URL
-  await page.pdf({path: 'output.pdf', format: 'A4'});
-  await browser.close();
-})();
